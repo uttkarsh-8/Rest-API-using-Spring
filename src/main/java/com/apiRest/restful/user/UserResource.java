db5a83cf8fa3f5fecc0c9368f58774d7570724cf
uttkarsh-8 <uttkarsh.srivastava12@gmail.com>
2024-02-29 00:06:48 +0530

    implemented Versioning of REST API, HATEOAS for allUsers()

--- src/main/java/com/apiRest/restful/user/UserResource.java
+++ src/main/java/com/apiRest/restful/user/UserResource.java
@@ -2,6 +2,8 @@ package com.apiRest.restful.user;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -20,15 +22,28 @@ public class UserResource {
         return service.findAll();
     }
 
+    // going to implement Hateos
+    //http://localhost:8080/users
+
+    // 2 things we have to use for Heateos is EntityModel and WebMvcLinkBuilder
+    // we are wrapping the User in EntityModel
     @GetMapping("/users/{id}")
-    public User retrieveUsers(@PathVariable int id){
+    public EntityModel<User> retrieveUsers(@PathVariable int id){
 
         User user = service.findOne(id);
 
         if (user==null){
             throw new UserNotFoundException("id: "+ id);
         }
-        return user;
+
+        EntityModel<User> entityModel = EntityModel.of(user);
+
+        WebMvcLinkBuilder link = WebMvcLinkBuilder
+                .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+
+        entityModel.add(link.withRel("all-user"));
+
+        return entityModel;
     }
 
     //POST/ USERs
--- src/main/java/com/apiRest/restful/versioning/VersioningPersonController.java
+++ src/main/java/com/apiRest/restful/versioning/VersioningPersonController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
+    //URI based versioning
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson(){
         return new PersonV1("John Doe");
@@ -13,5 +14,38 @@ public class VersioningPersonController {
     @GetMapping("/v2/person")
     public PersonV2 getSecondVersionOfPerson(){
         return new PersonV2(new Name("Lamds","Oklama"));
+    }
+
+    //Param based versioning
+    @GetMapping(value = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter(){
+        return new PersonV1("John Doe");
+    }
+
+    @GetMapping(value = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter(){
+        return new PersonV2(new Name("Labda", "Ooklama"));
+    }
+
+    //Header based Versioning
+    @GetMapping(value = "/person", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader(){
+        return new PersonV1("John Doe");
+    }
+
+    @GetMapping(value = "/person", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeader(){
+        return new PersonV2(new Name("Labda", "Ooklama"));
+    }
+
+    //Media type versioning
+    @GetMapping(value = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader(){
+        return new PersonV1("John Doe");
+    }
+
+    @GetMapping(value = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader(){
+        return new PersonV2(new Name("Lamda", "OOklama"));
     }
 }
